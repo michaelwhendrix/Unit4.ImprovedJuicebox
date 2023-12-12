5642ef5ab86752aef840fed53b9f921bc1a2ecbe
michaelwhendrix <michael.w.hendrix@gmail.com>
2023-12-12 11:31:44 -0600

    added createUser and api point

--- db/user.cjs
+++ db/user.cjs
@@ -2,9 +2,29 @@ const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
+const createUser = async(body) => {
+    console.log(body);
+    try {
+        const oneUser = await prisma.user.create({
+            data:body   
+        });
+        return oneUser;
+    } catch (error) {
+       console.log(error); 
+    }
+}
+
 const getUsers = async() => {
     const allUsers = await prisma.user.findMany();
     return allUsers;
 }
 
-module.exports = { getUsers };
+const getUserByName = async(name) => {
+    const oneUser = await prisma.user.findUnique({
+        where: {
+            username: name
+        }
+    });
+    return oneUser;
+}
+module.exports = { createUser,getUsers,getUserByName };
--- server.cjs
+++ server.cjs
@@ -4,7 +4,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-const {getUsers} = require('./db/user.cjs');
+const {getUsers, createUser} = require('./db/user.cjs');
 
 app.use(express.json());
 app.use('/assets', express.static(__dirname + '/dist/assets'));
@@ -16,6 +16,17 @@ app.get('/', (req, res) => {
 app.get('/users', async(req, res) => {
     const allUsers = await getUsers();
     res.send(allUsers);
-})
+});
+
+app.post('/users',async(req, res) => {
+    try {
+        console.log(req.body);
+        const newUser = await createUser(req.body);
+        res.send(newUser);
+    } catch (error) {
+        console.log(error);
+    }
+    
+});
 
 app.listen(PORT, () => {console.log(`Listneing on port ${PORT}`)});
