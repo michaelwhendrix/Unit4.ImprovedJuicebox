1b8c25dcd0a38003bdc6ba82c97e721da001ec3c
michaelwhendrix <michael.w.hendrix@gmail.com>
2023-12-13 08:54:05 -0600

    frontend login worked on

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
+import  Login  from "./user";
 
 function App() {
-
+const [username, setUsername] = useState('');
+const [password, setPassword] = useState('');
   return (
     <>
       <h1>Improved Juicebox</h1>
+      <form onSubmit={Login}>
+        <input placeholder="username" value={username} onChange={(e) => {setUsername(e.target.value)}}/>
+        <input placeholder="password" value={password} onChange={(e) => {setPassword(e.target.value)}}/>
+        <button>Log In</button>
+      </form>
     </>
   )
 }
---
+++ src/user.jsx
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+const Login = async(e) => {
+    e.preventDefault();
+    try {
+        //const response = await axios.get('/users/login', body);
+        console.log('response');       
+    } catch (error) {
+        
+    }
+}
+export default Login;
